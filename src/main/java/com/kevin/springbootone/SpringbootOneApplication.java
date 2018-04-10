42500e3d4f34e500f7cd1177d805b942c710b539
Kevin <Kevin_developer@163.com>
2018-04-10 15:19:12 +0800

    合入原因：修改启动类名称。 修改人：kevin

--- src/main/java/com/kevin/springbootone/SpringbootOneApplication.java
+++ src/main/java/com/kevin/springbootone/SpringbootOneApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOneApplication {
+public class SpringbootOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOneApplication.class, args);
+    }
 }
